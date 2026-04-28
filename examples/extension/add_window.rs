fc3a6768585aeac24f243c1bc4f39e8158ee2b37
Jan Hohenheim <jan@hohenheim.ch>
2026-04-28 03:11:07 +0200

    Improve UI

--- examples/extension/add_window.rs
+++ examples/extension/add_window.rs
@@ -4,7 +4,7 @@ use bevy::prelude::*;
 use jackdaw::prelude::*;
 use jackdaw_feathers::{
     button::{ButtonProps, button},
-    tokens::{FONT_LG, FONT_MD},
+    tokens::FONT_MD,
 };
 
 fn main() -> AppExit {
@@ -34,39 +34,67 @@ impl JackdawExtension for WindowExampleExtension {
     }
 
     fn register(&self, ctx: &mut ExtensionContext) {
-        ctx.register_operator::<ElapsedSecondsOp>().register_window(
-            // New windows need to have a unique ID. Usually, this follows the pattern `{extension_id}.window` if it's a single window,
-            // otherwise `{extension_id}.{some_name}`.
-            WindowDescriptor::new("window_example.window")
-                .with_name("Example Window")
-                .with_default_area(DefaultArea::Left)
-                .with_build(|window| {
-                    // This method here is used exactly like `Commands::with_children`.
-                    // using `.spawn` will spawn a new entity as a child of the window.
-                    // While you can style your UI however you want, jackdaw comes with a set of pre-built themed widgets
-                    // that you can use to have a consistent look and feel. Here, we use the built-in `button` widget,
-                    // which is directly linked to an operator. Things like the label, tooltip, etc. are automatically
-                    // set up for us based on the operator definition.
-                    window.spawn((
-                        Text::new("Here's your very own window!"),
-                        TextFont::from_font_size(FONT_LG),
-                    ));
-                    window.spawn((
-                        Text::new("Click the button below to log the elapsed seconds"),
-                        TextFont::from_font_size(FONT_MD),
-                    ));
-                    window.spawn(button(ButtonProps::from_operator::<ElapsedSecondsOp>()));
-                }),
-        );
+        ctx.register_operator::<UpdateElapsedTimeLabelOp>()
+            .register_window(
+                // New windows need to have a unique ID. Usually, this follows the pattern `{extension_id}.window` if it's a single window,
+                // otherwise `{extension_id}.{some_name}`.
+                WindowDescriptor::new("window_example.window")
+                    // To see out new UI, the user first needs to open the window by clicking on Window -> Example Window
+                    .with_name("Example Window")
+                    // After adding the window, it will be docked on the left side of the editor by default.
+                    .with_default_area(DefaultArea::Left)
+                    .with_build(|window| {
+                        // This method here is used exactly like `Commands::with_children`.
+                        // using `.spawn` will spawn a new entity as a child of the window.
+                        // While you can style your UI however you want, jackdaw comes with a set of pre-built themed widgets
+                        // that you can use to have a consistent look and feel.
+                        window.spawn((
+                            Node {
+                                margin: UiRect::all(px(10.0)),
+                                ..default()
+                            },
+                            // This here is a simple row layout with two columns: the label and the elapsed time.
+                            children![
+                                (
+                                    Text::new("Time passed since application startup:"),
+                                    TextFont::from_font_size(FONT_MD),
+                                ),
+                                (
+                                    ElapsedTimeLabel,
+                                    Text::new("<press the button below to update>"),
+                                    TextFont::from_font_size(FONT_MD),
+                                ),
+                            ],
+                        ));
+                        // Here, we use the built-in `button` widget, which is directly linked to an operator.
+                        // Things like the label, tooltip, etc. are automatically set up for us based on the operator definition.
+                        window.spawn(button(
+                            ButtonProps::from_operator::<UpdateElapsedTimeLabelOp>(),
+                        ));
+                    }),
+            );
     }
 }
 
+#[derive(Component)]
+struct ElapsedTimeLabel;
+
 #[operator(
-    id = "window_example.elapsed_seconds",
-    label = "Log Elapsed Seconds",
-    description = "Logs the elapsed seconds since Jackdaw started."
+    id = "window_example.update_elapsed_time_label",
+    label = "Update time label",
+    description = "Updates the label with the amount of time that passed since Jackdaw started."
 )]
-fn elapsed_seconds(_: In<OperatorParameters>, time: Res<Time>) -> OperatorResult {
-    info!("Elapsed seconds: {}", time.elapsed_secs());
+fn update_elapsed_time_label(
+    _: In<OperatorParameters>,
+    time: Res<Time>,
+    mut label: Single<&mut Text, With<ElapsedTimeLabel>>,
+) -> OperatorResult {
+    let elapsed = time.elapsed();
+    ***label = format!(
+        "{}m {}s {}ms",
+        elapsed.as_secs() / 60,
+        elapsed.as_secs() % 60,
+        elapsed.as_millis()
+    );
     OperatorResult::Finished
 }
